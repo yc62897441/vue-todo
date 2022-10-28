46ee590ebf89f99c447cb0a87dfd65bc6b3f3a41
yc62897441 <yc62897441@gmail.com>
2022-10-28 19:58:42 +0800

    完成 TodoView.vue

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,5 +1,106 @@
 <template>
-  <div>
-    <h1>This is the Todo Page.</h1>
+  <div class="container">
+    <div class="main-wrapper">
+      <div class="title-wrapper">
+        <h1>TODO List</h1>
+      </div>
+      <div class="input-wrapper">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" v-model="newTodo">
+          <button class="btn btn-primary" type="button" v-on:click="addTodo">新增</button>
+        </div>
+      </div>
+      <div class="todo-list-wrapper">
+        <div class="todo-wrapper" v-for="todo in todos" v-bind:key="todo.id">
+          <template v-if="todo.isCompleted">
+            <button type="button" class="btn btn-secondary" disabled>完成</button>
+            <div class="isCompleted">{{ todo.title }}</div>
+          </template>
+          <template v-else>
+            <button type="button" class="btn btn-success" v-on:click="finishTodo(todo.id)">完成</button>
+            <div>{{ todo.title }}</div>
+          </template>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import { v4 as uuidv4 } from 'uuid'
+
+export default {
+  data() {
+    return {
+      newTodo: '',
+      todos: []
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo === '') { return }
+      this.todos.push({
+        id: uuidv4(),
+        title: this.newTodo,
+        isCompleted: false
+      })
+      this.newTodo = ''
+    },
+    finishTodo(todoId) {
+      this.todos.forEach(todo => {
+        if (todo.id === todoId) {
+          todo.isCompleted = true
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.title-wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  margin-bottom: 1rem;
+}
+
+.form-control {
+  border-color: black;
+  border-radius: 0px;
+  margin-right: 2rem;
+}
+
+.todo-wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  margin-bottom: 1rem;
+}
+
+.btn {
+  height: 38px;
+  padding: 0px 25px;
+  border-radius: 0px;
+  color: black;
+  font-weight: 700;
+  margin-right: 0.5rem;
+}
+
+.btn-primary {
+  background-color: #b7defd;
+  border-color: #b7defd;
+}
+
+.btn-success {
+  background-color: #c9efd1;
+  border-color: #c9efd1;
+}
+
+.isCompleted {
+  color: gray;
+  text-decoration: line-through;
+}
+</style>
